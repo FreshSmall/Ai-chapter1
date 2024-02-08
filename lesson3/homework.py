ed702b117fed3cee02f157315bcf2c6e1c45e69b
yinchao <ycsuper2819@gmail.com>
2024-02-08 11:45:06 +0800

    paddlehub 模型增加分词方法

--- lesson3/homework.py
+++ lesson3/homework.py
@@ -31,5 +31,12 @@ def test1():
     print('result:{}'.format(result))
 
 
+def testLanguage():
+    lac = hub.Module(name="lac")
+    test_text = ["今天是个好天气。"]
+    results = lac.cut(text=test_text, use_gpu=False, batch_size=1, return_tag=True)
+    print(results)
+
+
 if __name__ == '__main__':
-    test1()
+    testLanguage()
